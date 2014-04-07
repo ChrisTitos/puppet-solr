0668ee8403ac1a01d2259b0722b719c2c14fb1bc
Venorcis <koeman@chainels.com>
2014-04-07 16:32:54 +0200

    More jetty > jetty-eclipse (package-dependency)

--- manifests/config.pp
+++ manifests/config.pp
@@ -27,14 +27,14 @@ class solr::config(
   file { '/etc/default/jetty':
     ensure  => file,
     source  => 'puppet:///modules/solr/jetty-default',
-    require => Package['jetty'],
+    require => Package['jetty-eclipse'],
   }
 
   file { $solr_home:
     ensure    => directory,
     owner     => 'jetty',
     group     => 'jetty',
-    require   => Package['jetty'],
+    require   => Package['jetty-eclipse'],
   }
 
   # download only if WEB-INF is not present and tgz file is not in /tmp:
@@ -69,7 +69,7 @@ class solr::config(
     owner     => 'jetty',
     group     => 'jetty',
     mode      => '0700',
-    require   => Package['jetty'],
+    require   => Package['jetty-eclipse'],
   }
 
   file { "${solr_home}/solr.xml":

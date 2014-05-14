daa2b947d6ad6676a4d8a4819ad31b58813cee81
Vincent Koeman <koeman@chainels.com>
2014-05-14 11:34:28 +0200

    no message

--- manifests/config.pp
+++ manifests/config.pp
@@ -52,7 +52,7 @@ class solr::config(
     path      =>  ['/usr/bin', '/usr/sbin', '/bin'],
     command   =>  "mv /tmp/solr-${solr_version} ${solr_home}",
     cwd       =>  $solr_home,
-    onlyif    =>  "test ! -d ${solr_home}/dist",
+    onlyif    =>  "test -d /tmp/solr-${solr_version} test ! -d ${solr_home}/dist",
     require   =>  Exec['extract-solr'],
   }
 
